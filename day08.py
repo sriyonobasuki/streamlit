cde73584cfb5f7ade2dceaa2093ff03aea3530df
sbasuki.tech <73261435+sriyonobasuki@users.noreply.github.com>
2024-02-18 05:14:18 +0700

    Add files via upload

---
+++ day08.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from datetime import time, datetime
+
+st.header('st.slider')
+
+# Example 1
+
+st.subheader('Slider')
+
+age = st.slider('How old are you?', 0, 150, 30)
+st.write("I'm ", age, 'years old')
+
+# Example 2
+
+st.subheader('Range slider')
+
+values = st.slider(
+     'Select a range of values',
+     0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+# Example 3
+
+st.subheader('Range time slider')
+
+appointment = st.slider(
+     "Schedule your appointment:",
+     value=(time(9, 0), time(12, 45)))
+st.write("You're scheduled for:", appointment)
+
+# Example 4
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="yyyy-MM-DD - hh:mm")
+st.write("Start time:", start_time)
